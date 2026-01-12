c875acd7fb5e9ba3b698fbac87b03818199ea06e
Xavier Goss <XGoss@driverse.com>
2026-01-12 17:06:37 -0500

    Added logic to save data

--- TripPlanner/Pages/NewVacationForm.xaml.cs
+++ TripPlanner/Pages/NewVacationForm.xaml.cs
@@ -2,9 +2,20 @@ namespace TripPlanner.Pages;
 
 public partial class NewVacationPage : ContentPage
 {
+	private readonly NewVacationPageModel _model;
+
 	public NewVacationPage(NewVacationPageModel model)
 	{
 		InitializeComponent();
-		BindingContext = model;
+		_model = model;
+		BindingContext = _model;
 	}
+
+    private void OnSaveClicked(object sender, EventArgs e)
+    {
+		var isValid = dataForm.Validate();
+
+		if (isValid)
+			_model.SubmitCmd.Execute(_model.Vacation);
+    }
 }
